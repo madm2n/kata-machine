29fb46bb7ec4e4b228ebae5cf8b8140b062782a5
Stepan Protsak <stephan.protsack@gmail.com>
2025-04-27 20:01:45 +0200

    Initial implementation of binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,22 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let haystack_pointer = haystack;
 
+    while (true) {
+        const middle_idx = Math.floor(haystack_pointer.length / 2);
+        const middle_elem = haystack_pointer[middle_idx];
+
+        if (middle_elem == needle) {
+            return true;
+        }
+
+        if (haystack_pointer.length == 1) {
+            return false;
+        }
+
+        if (middle_elem > needle) {
+            haystack_pointer = haystack_pointer.slice(0, middle_idx);
+        } else {
+            haystack_pointer = haystack_pointer.slice(middle_idx);
+        }
+    }
 }
