f58d76189ba0f34349cac2f54a9f4b74092a6626
Stepan Protsak <stephan.protsack@gmail.com>
2025-04-29 17:38:06 +0200

    Add proper BubbleSort implemenetation

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,9 +1,9 @@
 export default function bubble_sort(arr: number[]): void {
     for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j <= arr.length; j++) {
-            if (arr[i] < arr[j]) {
-                const tmp = arr[i];
-                arr[i] = arr[j];
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                const tmp = arr[j + 1];
+                arr[j + 1] = arr[j];
                 arr[j] = tmp;
             }
         }
