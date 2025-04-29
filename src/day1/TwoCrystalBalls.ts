b2a8d5651fa2d05092f2aeb0e14a37638ce83b1d
Stepan Protsak <stephan.protsack@gmail.com>
2025-04-29 17:30:11 +0200

    Add implemenation of BubbleSort and Balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,24 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jumpAmount = Math.floor(Math.sqrt(breaks.length));
 
+    let i = jumpAmount;
+
+    for (; i < breaks.length; i += jumpAmount) {
+        // find the first ball
+        if (breaks[i]) {
+            break;
+        }
+    }
+
+    // go back to the last known jump point
+    // search for the second ball
+    i -= jumpAmount;
+
+    for (let j = 0; j < jumpAmount; j++, i++) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
+
+    return -1;
 }
