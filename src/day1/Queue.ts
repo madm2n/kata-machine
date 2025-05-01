30a278ef97a364340c8255d3edfdc90b620338fd
Stepan Protsak <stephan.protsack@gmail.com>
2025-05-01 21:35:49 +0200

    Add initial queue implementaiton

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,44 @@
+interface Node<T> {
+    value: T;
+    next: Node<T> | undefined;
+}
+
 export default class Queue<T> {
-    public length: number;
-
-    
-
-    constructor() {
-    }
+    public length: number = 0;
+    private head: Node<T> | undefined;
+    private tail: Node<T> | undefined;
 
     enqueue(item: T): void {
+        const node: Node<T> = {
+            value: item,
+            next: undefined,
+        };
 
-}
+        if (!this.tail) {
+            this.head = node;
+            this.tail = node;
+        } else {
+            this.tail.next = node;
+            this.tail = node;
+        }
+
+        ++this.length;
+    }
     deque(): T | undefined {
+        const value = this.head?.value;
 
-}
+        if (this.head == this.tail) {
+            this.head = undefined;
+            this.tail = undefined;
+            this.length = 0;
+        } else {
+            this.head = this.head?.next;
+            --this.length;
+        }
+
+        return value;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
