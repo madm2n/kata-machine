f9f79b2cfa3581d523d61d041da589c087c0754e
Stepan Protsak <stephan.protsack@gmail.com>
2025-05-03 09:38:09 +0200

    Add initial stack implementation

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,34 @@
+type Node<T> = {
+    prev?: Node<T>;
+    value: T;
+};
+
 export default class Stack<T> {
-    public length: number;
-
-    
-
-    constructor() {
-    }
+    public length: number = 0;
+    private head?: Node<T>;
 
     push(item: T): void {
-
-}
+        const node: Node<T> = {
+            value: item,
+        };
+        this.length++;
+        node.prev = this.head;
+        this.head = node;
+    }
     pop(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
 
-}
+        const node = this.head;
+        this.head = node.prev;
+        node.prev = undefined;
+
+        this.length--;
+
+        return node.value;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
