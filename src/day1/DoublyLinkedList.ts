18e2d6c6fecb128f6e262e933a490f546f827792
Stepan Protsak <stephan.protsack@gmail.com>
2025-05-11 10:42:11 +0200

    Add insert at implementation

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -6,28 +6,70 @@ type Node<T> = {
 
 export default class DoublyLinkedList<T> {
     public length: number = 0;
-    private head?: Node<T>ƒ;
+    private head?: Node<T>;
 
     constructor() {}
 
     prepend(item: T): void {
-      const node: Node<T> = {
-        value: item,
-      }
-      this.length++;
+        const node: Node<T> = {
+            value: item,
+        };
+        this.length++;
 
-      if (!this.head) {
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
+
+        node.next = this.head;
+        this.head.prev = node;
         this.head = node;
-        return;
-      }
-
-      node.next = this.head;
-      this.head.prev = node;
-      this.head = node;
     }
-    insertAt(item: T, idx: number): void {}
+    insertAt(item: T, idx: number): void {
+        if (idx > this.length || idx < 0) {
+            throw new Error("Index out of range!");
+        }
+
+        if (idx === this.length) {
+            return this.prepend(item);
+        }
+
+        if (idx === 0) {
+            return this.append(item);
+        }
+
+        let node: Node<T> | undefined = this.head;
+
+        for (let i = 0; i < idx; i++) {
+            node = node?.next;
+        }
+
+        if (!node) {
+            return;
+        }
+
+        this.length++;
+
+        const newNode: Node<T> = {
+            value: item,
+            next: node,
+            prev: node.prev,
+        };
+
+        if (node.prev) {
+            node.prev.next = newNode;
+        }
+
+        node.prev = newNode;
+    }
     append(item: T): void {}
-    remove(item: T): T | undefined {}
-    get(idx: number): T | undefined {}
-    removeAt(idx: number): T | undefined {}
+    remove(item: T): T | undefined {
+        return undefined;
+    }
+    get(idx: number): T | undefined {
+        return undefined;
+    }
+    removeAt(idx: number): T | undefined {
+        return undefined;
+    }
 }
