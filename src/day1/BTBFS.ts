004f0d52a45e503ad49843c2138f081ba304922c
Stepan Protsak <stephan.protsack@gmail.com>
2025-05-27 20:28:15 +0200

    Implement bfs

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,21 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const queue: Array<BinaryNode<number> | null> = [head];
 
+    while (queue.length > 0) {
+        const node = queue.pop();
+
+        if (node?.value === needle) {
+            return true;
+        }
+
+        if (node?.left) {
+            queue.push(node.left);
+        }
+
+        if (node?.right) {
+            queue.push(node.right);
+        }
+    }
+
+    return false;
 }
