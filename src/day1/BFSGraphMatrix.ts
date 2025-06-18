1a5b1aa2227cdae661d780a4a0a937f51a5a4f7d
Stepan Protsak <stephan.protsack@gmail.com>
2025-06-18 09:28:21 +0200

    Implement BFSGraphMatrix algorithm

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -1,3 +1,53 @@
-export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+export default function bfs(
+    graph: WeightedAdjacencyMatrix,
+    source: number,
+    needle: number,
+): number[] | null {
+    const seen = new Array(graph.length).fill(false);
+    seen[source] = true;
 
+    const prev = new Array(graph.length).fill(-1);
+    const queue: number[] = [source];
+    let curr: number = -1;
+    let found: boolean = false;
+
+    do {
+        curr = queue.pop() as number;
+
+        if (curr === needle) {
+            found = true;
+            break;
+        }
+
+        const children: number[] = graph[curr];
+
+        for (let i = 0; i < children.length; i++) {
+            const weight = children[i];
+
+            if (weight == 0) {
+                continue;
+            }
+
+            if (seen[i]) {
+                continue;
+            }
+
+            seen[i] = true;
+            prev[i] = curr;
+            queue.push(i);
+        }
+    } while (queue.length > 0);
+
+    if (!found) {
+        return null;
+    }
+
+    const path: number[] = [];
+
+    while (curr != -1) {
+        path.unshift(curr);
+        curr = prev[curr];
+    }
+
+    return path;
 }
